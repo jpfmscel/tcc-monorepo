fdd84145790e34fe34c922ce77104089f5260db7
Joao Paulo Freitas <joaopaulo.freitas@adidas.com>
2022-05-02 14:58:44 +0200

    Adding eureka server

---
+++ eureka-server/src/test/java/com/jp/tcc/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.tcc.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EurekaServerApplicationTests {
+
+	@Test
+	public void contextIsLoaded() {
+	}
+
+}
